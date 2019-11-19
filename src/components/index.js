ef4b4e9acb11dfede11bd627b74683bb0c9a3144
arjitj87 <arjit.jindal@unacademy.com>
2019-11-19 15:28:29 +0530

    Renamed

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-import Test from './BaseComponent/TestComponent/TestComponent';
+import Test from './Base/TestComponent/TestComponent';
 import Clock from './Clock';
 import Page from './Page';
 import AddCount from './AddCount';
 import Links from './Links';
-import H4 from './BaseComponent/H4/H4';
-import H5 from './BaseComponent/H5/H5';
-import H6 from './BaseComponent/H6/H6';
+import H4 from './Base/H4';
+import H5 from './Base/H5';
+import H6 from './Base/H6';
 
 export { Clock, Page, AddCount, Links, Test, H4, H5, H6 };
