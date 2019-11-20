8d9782414d68492c3530a31c25dba9a9fb4f4eba
Rahul Shrivastava <rahul3103@gmail.com>
2019-11-20 16:13:54 +0530

    single quotes only

--- src/styleConstants/constants/fonts.js
+++ src/styleConstants/constants/fonts.js
@@ -1,12 +1,12 @@
-export const H1 = `48px`;
-export const H2 = `32px`;
-export const H3 = `24px`;
-export const H4 = `20px`;
-export const H5 = `14px`;
-export const H6 = `12px`;
+export const H1 = '48px';
+export const H2 = '32px';
+export const H3 = '24px';
+export const H4 = '20px';
+export const H5 = '14px';
+export const H6 = '12px';
 
-export const P1 = `14px`;
-export const P2 = `12px`;
+export const P1 = '14px';
+export const P2 = '12px';
 
 export const HEADER_LINE_HEIGHT = 1.5;
 export const PARA_LINE_HEIGHT = 1.2;
