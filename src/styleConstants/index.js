666be97ee87dce4d4555b977c5dec6a45e9aaded
priya <priya.pandian@unacademy.com>
2019-11-20 11:06:09 +0530

    adding constants

--- src/styleConstants/index.js
+++ src/styleConstants/index.js
@@ -1,3 +1,9 @@
+import * as Fonts from './constants/fonts';
+import * as Colors from './constants/colors';
+import * as Spacings from './constants/spacings';
+
+export { Fonts, Colors, Spacings };
+
 export default {
   colors: { primary: '#0070f3' },
   weights: { bold: 500, bolder: 600, boldest: 700 }
