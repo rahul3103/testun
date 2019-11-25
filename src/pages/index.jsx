975eef435192da5ea65ee33ecfcc0e3163163133
arjitj87 <arjit.jindal@unacademy.com>
2019-11-25 13:01:31 +0530

    code cleaned

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,9 @@
 /* eslint react/prop-types: 0 */
 
 import { PureComponent } from 'react';
-
-// import { useSelector } from 'react-redux';
 import { connect } from 'react-redux';
 import { fetchFreeCourses } from '../redux/actions';
-import Button from '../components/Base/Button/Button';
 import Layout from '../layout';
-
-// const Index = () => {
-//   const authors = useSelector(state => state.authors.data);
-//   const goals = useSelector(state => state.goals.data);
-//   return (
-//     <Layout>
-//       Authors
-//       <div style={{ gridColumn: 'span 18' }}>{JSON.stringify(authors)}</div>
-//       Goals
-//       <div style={{ gridColumn: 'span 18' }}>{JSON.stringify(goals)}</div>
-//     </Layout>
-//   );
-// };
-
-// Index.getInitialProps = async ({ store, isServer }) => {
-//   await store.dispatch(fetchFreeCourses());
-//   return { isServer };
-// };
-
-// export default Index;
 
 class Index extends PureComponent {
   componentDidMount() {
