ef62d5c3f4e67d5daff899d529970b94f3ee1f67
Apoorv <apoorv@Apoorvs-MacBook-Pro.local>
2019-11-26 11:26:57 +0530

    Index file unchanged, avatar changes done

--- src/components/Base/Avatar/Avatar.jsx
+++ src/components/Base/Avatar/Avatar.jsx
@@ -1,33 +1,19 @@
 import styled from 'styled-components';
-import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Spacings, BgColors } from '../../../styleConstants';
 
-const StyledImage = styled.div`
-  height: ${props => props.height};
-  width: ${props => props.width};
-  background-image: url('${props => props.imageUrl}');
+const Avatar = styled.div`
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
+  background-image: url('${({ src }) => src}');
   border-radius: ${Spacings.SPACING_2B};
-  background-size: ${props => props.imgMode};
+  background-size: ${({ imgMode }) => imgMode};
   background-repeat: no-repeat;
-  background-color: ${props => props.backgroundColor};
+  background-color: ${({ backgroundColor }) => backgroundColor};
 `;
 
-const Avatar = ({ imageUrl, height, width, backgroundColor, imgMode }) => {
-  return (
-    <StyledImage
-      imageUrl={imageUrl}
-      height={height}
-      width={width}
-      backgroundColor={backgroundColor}
-      imgMode={imgMode}
-    />
-  );
-};
-
 Avatar.propTypes = {
-  imageUrl: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
   height: PropTypes.string,
   width: PropTypes.string,
   backgroundColor: PropTypes.string,
